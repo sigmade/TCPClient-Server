e612e4542393c5fff4e5b2f56255c7dc0eea684f
sigmade <sychyov1991@mail.ru>
2020-05-06 05:47:49 +0300

    Add project files.

---
+++ TCP Client/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TCP_Client
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IpBox = new System.Windows.Forms.TextBox();
+            this.PortBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.ResponseBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP Сервера";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Порт сервер";
+            // 
+            // IpBox
+            // 
+            this.IpBox.Location = new System.Drawing.Point(170, 57);
+            this.IpBox.Name = "IpBox";
+            this.IpBox.Size = new System.Drawing.Size(175, 20);
+            this.IpBox.TabIndex = 2;
+            this.IpBox.Text = "127.0.0.1";
+            // 
+            // PortBox
+            // 
+            this.PortBox.Location = new System.Drawing.Point(170, 98);
+            this.PortBox.Name = "PortBox";
+            this.PortBox.Size = new System.Drawing.Size(175, 20);
+            this.PortBox.TabIndex = 3;
+            this.PortBox.Text = "6666";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Сообщение серверу";
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Location = new System.Drawing.Point(170, 150);
+            this.MessageBox.Multiline = true;
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(369, 94);
+            this.MessageBox.TabIndex = 5;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(170, 261);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 6;
+            this.button.Text = "Отправить";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // ResponseBox
+            // 
+            this.ResponseBox.Location = new System.Drawing.Point(170, 306);
+            this.ResponseBox.Multiline = true;
+            this.ResponseBox.Name = "ResponseBox";
+            this.ResponseBox.Size = new System.Drawing.Size(369, 94);
+            this.ResponseBox.TabIndex = 7;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 450);
+            this.Controls.Add(this.ResponseBox);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PortBox);
+            this.Controls.Add(this.IpBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form";
+            this.Text = "TCP Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IpBox;
+        private System.Windows.Forms.TextBox PortBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox MessageBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox ResponseBox;
+    }
+}
+
---
+++ TCP Client/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_Client
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+             string ip = IpBox.Text;
+             int port = Convert.ToInt32(PortBox.Text);
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var message = MessageBox.Text;
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+
+            ResponseBox.Text = answer.ToString();
+
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+        }
+    }
+}
